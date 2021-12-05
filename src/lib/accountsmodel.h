bc3aaf7db5a379a9c3274256975043f5c835637f
Nicolas Fella <nicolas.fella@gmx.de>
2021-12-05 01:35:54 +0100

    [accountsmodel] Expose roles enum to QML

--- src/lib/accountsmodel.h
+++ src/lib/accountsmodel.h
@@ -46,6 +46,7 @@ public:
         DataObjectRole,
         ProviderDisplayNameRole,
     };
+    Q_ENUM(Roles);
     explicit AccountsModel(QObject *parent = nullptr);
     virtual ~AccountsModel();
 
